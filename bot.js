45ffc188e0196fd8201d475742cf46804340165d
Dentbot <45294268+Dentbot@users.noreply.github.com>
2018-11-23 20:04:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,4 +14,4 @@ client.user.setStatus("dnd")
 
 
 
-client.login(process.env.NDU5OTM3NTQ5NjE2NDE0NzMw.DtCYvw.yBEWakrNC9EbeZDvIcTbyIy_Ys4);// لا تغير فيها شيء
+client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
